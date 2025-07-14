3e58dbb9c580153147b1f932cdf5c8da3607601b
AshiwaniKumar-hacker <kumarashiwani532@gmail.com>
2025-07-14 19:08:01 +0530

    add css to food app change color of item when clicked by the use of useState

--- 18-fragments/learning-fragments/src/components/Item.jsx
+++ 18-fragments/learning-fragments/src/components/Item.jsx
@@ -1,9 +1,9 @@
 import  styles from "./Item.module.css";
 
-const Item=({foodItem,handleBuyButton})=>{
+const Item=({foodItem,bought,handleBuyButton})=>{
 
 return (
-<li  className={`${styles['kg-item']} list-group-item`}> <span className={styles['kg-span']}>{foodItem}</span>
+<li  className={`${styles['kg-item']} list-group-item  ${bought && "active"}`}> <span className={styles['kg-span']}>{foodItem}</span>
 <button className={`${styles.button} btn btn-info`}
 onClick={handleBuyButton}
 >Buy</button>
