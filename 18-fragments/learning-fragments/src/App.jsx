5a70390487cd034a1cb58a6f0ce9359f577f4418
AshiwaniKumar-hacker <kumarashiwani532@gmail.com>
2025-07-14 13:31:50 +0530

    learn useState and apply it on Food App

--- 18-fragments/learning-fragments/src/App.jsx
+++ 18-fragments/learning-fragments/src/App.jsx
@@ -1,25 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 import FoodItems from "./components/Fooditems";
 import ErrorMessage from "./components/ErrorMessage";
 import Container from "./components/Container";
+import FoodInput from "./components/FoodInput";
 
 function App() {
 
-   let foodItems =['DAL','FRUITS','ROTI','RICE','VIGETABLES'];
+//    let textStateArr= useState("Food Item Entered by user.");
+//    let textToShow=textStateArr[0];
+//    let setTextState=textStateArr[1];  
 
+let[foodItems, setFoodItems]=useState([]);
+
+
+const onKeyDown=(event)=>{
+     if(event.key==='Enter') {
+          let newFoodItem = event.target.value;
+          event.target.value='';
+          let newItems=[...foodItems,newFoodItem];
+          setFoodItems(newItems);
+          console.log(newFoodItem);
+     }
+}
     
   return ( <>
   <Container>
   <h1 className="food-heading">Healthy Food</h1>
-   <ErrorMessage items={foodItems}></ErrorMessage>
+  <FoodInput handleKeyDown={onKeyDown}></FoodInput>
+  <ErrorMessage items={foodItems}></ErrorMessage>
   <FoodItems items={foodItems}></FoodItems>
          </Container>
-
-         <Container>
+         
+{/* 
+          <Container>
               <p>Above is the list of healthy foods that are good for health and well being</p>
-         </Container>
+         </Container> */}
+
+
  </> )
 }
 
